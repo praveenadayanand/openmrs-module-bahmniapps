cee898f65357c15e29067e417af378bf858caa79
Praveena Dayanand <praveena.dayanand@thoughtworks.com>
2020-04-06 17:33:01 +0530

    BAH-990 | Praveena | Translated attribute field names

--- ui/app/common/attributeTypes/directives/attributeTypes.js
+++ ui/app/common/attributeTypes/directives/attributeTypes.js
@@ -1,6 +1,10 @@
 'use strict';
 
-angular.module('bahmni.common.attributeTypes', []).directive('attributeTypes', [function () {
+angular.module('bahmni.common.attributeTypes', []).directive('attributeTypes', ['$translate', function ($translate) {
+    var modulePrefixMap = { 'registration': 'REGISTRATION_ATTRIB_',
+                            'program' : 'PROGRAM_ATTRIBUTE_',
+                            'OT' : 'OT_'
+                            };
     return {
         scope: {
             targetModel: '=',
@@ -15,7 +19,7 @@ angular.module('bahmni.common.attributeTypes', []).directive('attributeTypes', [
         },
         templateUrl: '../common/attributeTypes/views/attributeInformation.html',
         restrict: 'E',
-        controller: function ($scope) {
+        controller: function ($scope, $translate) {
             $scope.getAutoCompleteList = $scope.getAutoCompleteList();
             $scope.getDataResults = $scope.getDataResults();
             // to avoid watchers in one way binding
@@ -30,6 +34,19 @@ angular.module('bahmni.common.attributeTypes', []).directive('attributeTypes', [
                 });
                 attributeValueConceptType.value = concept && concept.fullySpecifiedName;
             };
+            $scope.translateAttributeName = function(attribute, moduleName) {
+                var keyPrefix = moduleName && modulePrefixMap[moduleName] ? modulePrefixMap[moduleName] : '';
+                keyPrefix = (keyPrefix == '' && attribute.keyPrefix) ? attribute.keyPrefix : '';
+
+                var keyName = attribute.name.toUpperCase().replace(/\s\s+/g, ' ').replace(/[^a-zA-Z0-9 _]/g, "").trim().replace(/ /g,"_")
+                var translationKey = keyPrefix+keyName;
+                var translation = $translate.instant(translationKey);
+                if (translation == translationKey) {
+                    return attribute.description;
+                }
+                return translation;
+            }
+
         }
     };
 }]);
